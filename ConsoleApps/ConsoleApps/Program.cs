f965a37a483a0ee0c80ad9e08aa50a42745a9146
mohasy <hot.papa@hotmail.fr>
2016-10-21 17:00:08 +0000

    message bonjour

--- ConsoleApps/ConsoleApps/Program.cs
+++ ConsoleApps/ConsoleApps/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-
+            System.Console.WriteLine("hello world");
+            System.Console.WriteLine("Bonjour la famille");
         }
     }
 }
